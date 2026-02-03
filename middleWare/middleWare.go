3d78a9e030a5faab7e01a9856114b33d2cd66e42
zhangbojia <3632743658@qq.com>
2026-02-03 08:18:06 +0800

    提交

--- middleWare/middleWare.go
+++ middleWare/middleWare.go
@@ -41,3 +41,64 @@ func MiddleWare() gin.HandlerFunc {
 }
 
 //handler, err := middleWare.TokenHandler(strconv.FormatInt(login.UserId, 10), time.Now().Add(time.Hour*time.Duration(1)).Unix())
+/*
+var good model.Goods
+	err := good.FindGoodsByTitle(config.DB, in.Title)
+	if err == nil {
+		return nil, errors.New("数据不能重入库")
+	}
+	goods := model.Goods{
+		Title:   in.Title,
+		Url:     in.Title,
+		Content: in.Content,
+		Stock:   int(in.Stock),
+		Price:   float64(in.Price),
+	}
+	begin := config.DB.Begin()
+	err = goods.GoodsAdd(config.DB)
+	if err != nil {
+		begin.Rollback()
+		return nil, errors.New("商品上架失败")
+	}
+
+	key := fmt.Sprintf("goods:%s", in.Title)
+	marshaler, err := json.Marshal(goods)
+	if err != nil {
+		begin.Rollback()
+		return nil, errors.New("商品序列化失败")
+	}
+	nx := config.Rdb.SetNX(config.Ctx, key, marshaler, time.Minute*30).Val()
+	if !nx {
+		begin.Rollback()
+		return nil, errors.New("商品已存在,请勿重复上架")
+	}
+	begin.Commit()
+	goodsMap := map[string]interface{}{
+		"id":      goods.ID,
+		"title":   in.Title,
+		"url":     in.Title,
+		"content": in.Content,
+		"stock":   int(in.Stock),
+		"price":   float64(in.Price),
+	}
+	_, err = config.Es.Index().Index("goods_map").BodyJson(goodsMap).Do(context.Background())
+	if err != nil {
+		return nil, errors.New("es同步失败")
+	}
+*/
+/*
+var detail model.Goods
+	key := fmt.Sprintf("goods:%s", in.Title)
+	err := config.Rdb.Exists(config.Ctx, key).Val()
+	if err == 0 {
+		return nil, errors.New("商品不存在")
+	} else {
+		result, _ := config.Rdb.Get(config.Ctx, key).Result()
+		json.Unmarshal([]byte(result), &detail)
+	}
+*/
+/*
+ proto, _ := cmd.Flags().GetString("proto")
+ exec.Command("protoc",proto,"--go_out=.","--go-grpc_out=.",).Run()
+ pbCmd.Flags().StringVarP(&proto, "proto", "p", "", "生成proto文件")
+*/
